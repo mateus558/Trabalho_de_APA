6d1c01c7dbfaa322edf0caf24f98a4ea9f9f06fe
rafaelstjf <rafaelstjf@hotmail.com>
2017-04-19 17:41:31 -0300

    readme

--- sorting.h
+++ sorting.h
@@ -1,9 +1,9 @@
 #ifndef SORTING_H_INCLUDED
 #define SORTING_H_INCLUDED
-private:
+
 void swap(int v[], int a, int b);
 int partition(int v[], int low, int high);
-public:
+
 void bubble_sort(int v[], int n);
 void selection_sort(int v[], int n);
 void insertion_sort(int v[], int n);
