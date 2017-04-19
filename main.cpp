b30f014cab0e698fc1cecc9194553d0b26ffe760
rafaelstjf <rafaelstjf@hotmail.com>
2017-04-19 17:02:19 -0300

    Recursive Quicksort

--- main.cpp
+++ main.cpp
@@ -8,14 +8,19 @@ using namespace std;
 
 int main(){
 	int v[] = {6, 5, 4, 3, 2, 1, -1, -2, 0};
+	int v1[] = {3,1,2, -1};
 	int i;
-	
+
 	cout << "original: ";
 	for(i = 0; i < N; ++i) cout << v[i] << " ";
 	cout << endl;
-	bubble_sort(v, 9);
+	/*bubble_sort(v, 9);
 	cout << "ordenado: ";
 	for(i = 0; i < N; ++i) cout << v[i] << " ";
+	cout << endl;*/
+	quick_sort(v,0, 9);
+	cout << "quick_sort" << endl;
+	for(i = 0; i< 9; i++) cout << v[i] << " ";
 	cout << endl;
 
 }
--- sorting.cpp
+++ sorting.cpp
@@ -3,7 +3,7 @@
 * Purpose: Implement algorithms to sort arrays of integers
 *
 * @author Mateus Coutinho Marim
-* @version 1.0 23/03/17 
+* @version 1.0 23/03/17
 */
 
 #include "sorting.h"
@@ -14,7 +14,7 @@ using namespace std;
 
 /*!
 	Swaps two elements in a vector.
-	
+
 	@param v is an c-like array.
 	@param a is the position of the first element to be swaped.
 	@param b is the position of the second element to be swaped.
@@ -29,16 +29,32 @@ void swap(int v[], int a, int b){
 
 /*!
 	Partition step of quick sort algorithm.
-	
+
 	@param v is an c-like array.
 	@param low is the lowest position of the subarray.
 	@param high is the highest position of the subarray.
-	@return the pivot of the subarray.
+	@return the pivot's position of the subarray.
 */
 
 int partition(int v[], int low, int high){
-  
+	int pivot, i, j;
+	pivot = v[low];
+	i = low;
+	j = high;
+	while(i < j){
+		while(v[i] <=pivot)
+		i++;
+		while(v[j]>pivot)
+		j--;
+		if(i<j)
+		swap(v, i, j);
+
+	}
+	v[low] = v[j];
+	v[j] = pivot;
+	return j;
 }
+
 
 /*!	Implementation of the quick sort algorithm
 
@@ -49,7 +65,11 @@ int partition(int v[], int low, int high){
 */
 
 void quick_sort(int v[], int low, int high){
-	
+	if(low<high){
+		int pivot = partition(v, low, high);
+		quick_sort(v, low, (pivot-1));
+		quick_sort(v, (pivot+1), high);
+	}
 }
 
 /*!	Implementation of the bubble sort algorithm
@@ -61,7 +81,7 @@ void quick_sort(int v[], int low, int high){
 
 void bubble_sort(int v[], int n){
 	int i, j, swaps;
-	
+
 	for(i = 0; ; ++i){
 		swaps = 0;
 		for(j = 0; j < n-1; ++j){
@@ -82,7 +102,7 @@ void bubble_sort(int v[], int n){
 */
 void selection_sort(int v[], int n){
 	int i, j, min, pos_min;
-	
+
 	for(i = 0; i < n-1; i++){
 		for(j = i + 1, min = v[i], pos_min = i; j < n; j++){
 			if(v[j] < min){
@@ -91,9 +111,9 @@ void selection_sort(int v[], int n){
 			}
 		}
 		v[pos_min] = v[i];
-		v[i] = min;  
+		v[i] = min;
 	}
-}	
+}
 
 /*! Implementation of the insertion sort algorithm.
 
@@ -103,13 +123,13 @@ void selection_sort(int v[], int n){
 */
 void insertion_sort(int v[], int n){
 	int i, j, x;
-	
+
 	for(i = 1; i < n; i++){
 		x = v[i];
-				
+
 		for(j = i - 1; j >= 0 && v[j] > x; --j){
 			v[j + 1] = v[j];
-			v[j] = x;	
+			v[j] = x;
 		}
 	}
 }
