bb300afec30100247cbb88ffea5034c6c28add4b
rafaelstjf <rafaelstjf@hotmail.com>
2017-04-21 11:09:30 -0300

    ListGenerator.py update :+1:

--- List_Generator.py
+++ List_Generator.py
@@ -1,19 +1,36 @@
 from random import randint #library to generate random numbers
 def randomName():
-    names = ['Rafael', 'Mateus', 'Aleksander', 'Mattheus', 'Gabriel', 'Carlos', 'Maria', 'Laura', 'Paula', 'Roberta']
+    names = ['Rafael', 'Mateus', 'Aleksander', 'Mattheus', 'Gabriel', 'Carlos', 'Maria', 'Laura', 'Paula', 'Roberta', 'Adriel', 'Edmar', 'Marcela', 'Robson']
+    lastNames = ['Souza', 'Santos', 'Oliveira', 'Silva', 'Ribeiro', 'Amaral', 'Lima', 'Carvalho', 'Ferreira', 'Costa', 'Gomes', 'Ribeiro', 'Teixeira', 'Rocha']
     index = randint(0, (len(names)-1))
-    return names[index]
+    return (names[index]+ ' ' + lastNames[index])
 
-def generateData(n):
-    for i in range (0,n+1,2):
-        file.write(str(i) + '\n')
-        file.write(randomName() + '\n')
-    for j in range(n-1, 0, -2):
-        file.write(str(j) + '\n')
-        file.write(randomName() + '\n')
+def generateList(n):
+    fullName = []
+    for i in range (0, n):
+        fullName.append(randomName())
+    #list without repeated elements
+    for i in range (0,n-1,2):
+        ListNoR.write(str(i) + '\n')
+        ListNoR.write(fullName[i] + '\n')
+    for i in range(n-1, 0, -2):
+        ListNoR.write(str(i) + '\n')
+        ListNoR.write(fullName[i] + '\n')
+    #list in ascending order
+    for k in range(0, n):
+        ListAOrd.write(str(i) + '\n')
+        ListAOrd.write(fullName[i] + '\n')
+    #list in descending order
+    for i in range(n-1, -1, -1):
+        ListDOrd.write(str(i) + '\n')
+        ListDOrd.write(fullName[i] + '\n')
 
 #main
-file = open('List.txt', 'w')
+ListNoR = open('ListNoR.txt', 'w') #list without repeated elements
+ListAOrd = open('ListAOrd.txt', 'w') #list in ascending order
+ListDOrd = open('ListDOrd.txt', 'w')#List in descending order
 n = input("What is the size of the list?\n")
-generateData(n)
-file.close()
+generateList(n)
+ListNoR.close()
+ListAOrd.close()
+ListDOrd.close()
