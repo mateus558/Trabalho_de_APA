0d7a1ebff2ab45bb9c750e50b1b81ee258d47870
rafaelstjf <rafaelstjf@hotmail.com>
2017-04-22 13:34:20 -0300

    Open file with arguments

--- main.cpp
+++ main.cpp
@@ -7,27 +7,30 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char * argv []){
 	Node *v2 = NULL;
-	string path("ListRO100.txt");
-	int v[] = {6, 5, 4, 3, 2, 1, -1, -2, 0};
-	int v1[] = {3,1,2, -1};
-	int i, n;
+	if(argc!=2)
+		return -1;
+	else{
+		string path(argv[1]);
+		int v[] = {6, 5, 4, 3, 2, 1, -1, -2, 0};
+		int v1[] = {3,1,2, -1};
+		int i, n;
 
-	n = load_list(path, &v2);
-	cout << v2[0].info << endl;
-	if(!v2) cout << "cu" << endl;
-	cout << "original: ";
-	print_nodes(v2, n);
-	cout << endl;
-	/*bubble_sort(v, 9);
-	cout << "ordenado: ";
-	for(i = 0; i < N; ++i) cout << v[i] << " ";
-	cout << endl;*/
-	//bubble_sort(v2, n);
-	quick_sort(v2, 0, n-1);
-	cout << "quick_sort" << endl;
-	print_nodes(v2, n);
-	cout << endl;
-
+		n = load_list(path, &v2);
+		cout << v2[0].info << endl;
+		if(!v2) cout << "cu" << endl;
+		cout << "original: ";
+		print_nodes(v2, n);
+		cout << endl;
+		/*bubble_sort(v, 9);
+		cout << "ordenado: ";
+		for(i = 0; i < N; ++i) cout << v[i] << " ";
+		cout << endl;*/
+		//bubble_sort(v2, n);
+		quick_sort(v2, 0, n-1);
+		cout << "quick_sort" << endl;
+		print_nodes(v2, n);
+		cout << endl;
+	}
 }
