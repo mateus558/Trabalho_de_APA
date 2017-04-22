b6d8be87b98195a3183cb519cec4d050eab66fa1
rafaelstjf <rafaelstjf@hotmail.com>
2017-04-22 17:07:02 -0300

    While to run all the Lists

--- Run.sh
+++ Run.sh
@@ -4,31 +4,32 @@ echo "Starting Experiments"
 make
 
 N=10
-
-echo "Size: $N"
-
-PATH1="List/ListAmOrd${N}.txt"
-PATH2="List/ListAOrd${N}.txt"
-PATH3="List/ListDOrd${N}.txt"
-PATH4="List/ListRO${N}.txt"
-
-for i in `seq 1 5`
+while [ $N -le 1000000 ]
 do
-	$(./main $PATH1 $i)
-done
+  echo "Size: $N"
+  PATH1="List/ListAmOrd${N}.txt"
+  PATH2="List/ListAOrd${N}.txt"
+  PATH3="List/ListDOrd${N}.txt"
+  PATH4="List/ListRO${N}.txt"
 
-for i in `seq 1 5`
-do
-	$(./main $PATH2 $i)
-done
+  for i in `seq 1 5`
+  do
+	 $(./main $PATH1 $i)
+ done
 
-for i in `seq 1 5`
-do
-	$(./main $PATH3 $i)
-done
+  for i in `seq 1 5`
+  do
+	   $(./main $PATH2 $i)
+  done
 
-for i in `seq 1 5`
-do
-	$(./main $PATH4 $i)
-done
+  for i in `seq 1 5`
+  do
+    $(./main $PATH3 $i)
+  done
 
+  for i in `seq 1 5`
+  do
+    $(./main $PATH4 $i)
+  done
+  let "N=N*10"
+done
