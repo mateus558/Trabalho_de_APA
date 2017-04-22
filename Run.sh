068f07db9c3db55105cffef244cbe6e0d6df92c9
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2017-04-22 16:37:14 -0300

    Script for experiments updated.

--- Run.sh
+++ Run.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 echo "Starting Experiments"
+
 make
+
 N=10
+
 echo "Size: $N"
-./main List/ListAmOrd${N}.txt 1
-./main List/ListAmOrd${N}.txt 2
-./main List/ListAmOrd${N}.txt 3
-./main List/ListAmOrd${N}.txt 4
-./main List/ListAmOrd${N}.txt 5
-./main List/ListAOrd${N}.txt 1
-./main List/ListAOrd${N}.txt 2
-./main List/ListAOrd${N}.txt 3
-./main List/ListAOrd${N}.txt 4
-./main List/ListAOrd${N}.txt 5
-./main List/ListDOrd${N}.txt 1
-./main List/ListDOrd${N}.txt 2
-./main List/ListDOrd${N}.txt 3
-./main List/ListDOrd${N}.txt 4
-./main List/ListDOrd${N}.txt 5
-./main List/ListRO${N}.txt 1
-./main List/ListRO${N}.txt 2
-./main List/ListRO${N}.txt 3
-./main List/ListRO${N}.txt 4
-./main List/ListRO${N}.txt 5
+
+PATH1="List/ListAmOrd${N}.txt"
+PATH2="List/ListAOrd${N}.txt"
+PATH3="List/ListDOrd${N}.txt"
+PATH4="List/ListRO${N}.txt"
+
+for i in `seq 1 5`
+do
+	$(./main $PATH1 $i)
+done
+
+for i in `seq 1 5`
+do
+	$(./main $PATH2 $i)
+done
+
+for i in `seq 1 5`
+do
+	$(./main $PATH3 $i)
+done
+
+for i in `seq 1 5`
+do
+	$(./main $PATH4 $i)
+done
+
